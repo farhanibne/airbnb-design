0af489951255a8c1a7f8c4d5cafe5b84c5dd0f22
farhan ibne saif <84982846+farhanibne@users.noreply.github.com>
2022-04-10 12:26:08 +0600

    Add files via upload

---
+++ build/precache-manifest.92a52e9153a1ea80b306ce90d52c3132.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a328f4ebcc36b385a6347b2cc6d2b7a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12005095b55a87d2598f",
+    "url": "/static/css/2.51457ed1.chunk.css"
+  },
+  {
+    "revision": "b928b7c76fd5a155b560",
+    "url": "/static/css/main.801bd4c3.chunk.css"
+  },
+  {
+    "revision": "12005095b55a87d2598f",
+    "url": "/static/js/2.b6a595c8.chunk.js"
+  },
+  {
+    "revision": "9efecc6e366a89e935d3722c67df4853",
+    "url": "/static/js/2.b6a595c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b928b7c76fd5a155b560",
+    "url": "/static/js/main.00d494d4.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
